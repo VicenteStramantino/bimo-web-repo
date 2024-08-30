3e7c9fbd1a536495205282bbb0e1a9a6e5621475
VicenteStramantino <vicente.stramantino@germinare.com.br>
2024-08-30 07:52:07 -0300

    Subindo models, controllers e services sem revisao

--- src/main/java/com/example/apibimo/models/Usuario.java
+++ src/main/java/com/example/apibimo/models/Usuario.java
@@ -17,54 +17,64 @@ public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int sId;
+    private int sid;
 
     @NotNull
     @Size(min = 2,message = "O nome deve ter no minimo 2 caracteres")
     @Schema(description = "Nome do usuário")
-    private String cNome;
+    private String cnome;
 
     @NotNull
     @Size(min = 3, message = "O sobrenome deve ter mai que 3 caracteres")
     @Schema(description = "Sobrenome utilizado pelo usuario")
-    private String cSobrenome;
+    private String csobrenome;
 
 
     @NotNull
     @Size(min = 4, message = "A senha deve conter pelo menos 4 caracteres.")
     @Schema(description = "Senha utilizada pelo usuario para logar.")
-    private String cSenha;
+    private String csenha;
 
     @NotNull
     @Size(min = 11, message = "Um caracter deve conter no minimo 11 caracteres.")
     @Schema( description = "Cpf ligado a conta do usuário.")
     @CPF
-    private String cCPF;
+    private String ccpf;
 
     @NotNull
     @Size(min = 7, message = "O email deve ter no Minimo 7 caracteres")
     @Schema(description = "E-mail ligada a conta do usuario")
     @Email
-    private String cEmail;
+    private String cemail;
 
 
     @NotNull
     @Size(min = 14, message = "O CNPJ deve ter no minimo 14 caracteres.")
     @CNPJ
     @Schema(description = "Caso o usuario seja uma empresa, este campo ira guardar o CNPJ da empresa")
-    private String cCNPJ;
+    private String ccnpj;
 
 
     @NotNull
     @Size(min = 11, message = "Um telefone deve ter no minimo 11 caracteres.")
     @Schema(description = "Telefone de contado que estara presente na conta do usuario")
-    private String cTelefone;
+    private String ctelefone;
 
     @NotNull
     @Size(min = 10, message = "A data de nascimento deve ter no minimo 10 caracteres")
     @Schema(description = "Data de nascimento do usuario.")
-    private String dDataNascimento;
+    private String ddatanascimento;
 
+
+    @NotNull
+    @Size(min = 8, message = "O link deve ter no minimo 8 caracteres")
+    @Schema(description = "Atributo onde ficaria guardado o link do Linkedin do usuário.")
+    private String  clinklinkedin;
+
+
+    @NotNull
+    @Schema(description = "Atributo onde ficaria guardado o hash para login do usuario. Este hash vira do database")
+    private String  cidhash;
 
 
 
--- src/main/java/com/example/apibimo/repository/CurriculoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.apibimo.repository;
-import com.example.apibimo.models.Curriculo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CurriculoRepository extends JpaRepository<Curriculo, Long> {
-
-}
